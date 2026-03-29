eacdf5439af15483a4c6fdf0989f50bfc93b5ec9
Rahul Arya <aryarahul819@gmail.com>
2026-03-29 13:28:20 +0530

    Update Homebrew formula SHA256

--- packaging/homebrew/wmc.rb
+++ packaging/homebrew/wmc.rb
@@ -2,7 +2,7 @@ class Wmc < Formula
   desc "Clean downloaded WhatsApp media on macOS"
   homepage "https://github.com/Rahularya01/wmc"
   url "https://github.com/Rahularya01/wmc/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "REPLACE_WITH_RELEASE_TARBALL_SHA256"
+  sha256 "1b95c4f7265e9cb0220a0d7b9532cc90688ac1b5c7f7a3c1712ec29638ee4d01"
   license "MIT"
 
   depends_on "rust" => :build
